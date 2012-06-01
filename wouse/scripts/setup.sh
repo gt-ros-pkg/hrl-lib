4e80b13ee2bc4f4574635642853c9047b4378031
Hai Nguyen <haidai@gmail.com>
2012-06-01 15:51:19 -0400

    started hrl_lib

--- wouse/scripts/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#Provide setup for wouse
-echo 'SUBSYSTEMS=="usb", KERNEL=="event3", SYSFS{idVendor}=="214e", SYSFS{idProduct}=="0001", NAME="wouse", MODE="0666", OPTIONS+="last_rule"' > /etc/udev/rules.d/wouse.rules
