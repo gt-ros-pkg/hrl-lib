29f149817058aab65edddde7be592fc6653efb31
Hai Nguyen <haidai@gmail.com>
2012-08-07 10:10:00 -0700

    added wait for transform

--- hrl_lib/src/hrl_lib/tf_utils.py
+++ hrl_lib/src/hrl_lib/tf_utils.py
@@ -12,6 +12,7 @@ def matrix_as_tf(mat):
     return (tr.translation_from_matrix(mat), tr.quaternion_from_matrix(mat))
 
 def transform(to_frame, from_frame, tflistener, t=0):
+    tflistener.waitForTransform(to_frame, from_frame, rospy.Time(t), rospy.Duration(10))
     return tf_as_matrix(tflistener.lookupTransform(to_frame, from_frame, rospy.Time(t)))
 
 def transform_points(T, points):
