07cdf9a3548e8d9010aeb7e4292fdf8edf3c82d8
Daehyung Park <deric.park@gatech.edu>
2015-10-13 11:01:07 -0700

    updated quat_normal

--- hrl_lib/src/hrl_lib/quaternion.py
+++ hrl_lib/src/hrl_lib/quaternion.py
@@ -69,7 +69,7 @@ def quat_inv_sign(q):
 
 # Return a normalized quaternion
 def quat_normal(q):
-    return q/np.norm(q)
+    return q/np.linalg.norm(q)
 
 # Return true or false whether two quaternion is on a same hemisphere.
 def AreQuaternionClose(q1,q2):
